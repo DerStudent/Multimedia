86ef2f7f53b652716335958c48e12cba92bff9e1
DerStudent <strolsu@googlemail.com>
2018-04-11 10:16:44 +0100

    Main methode erweitert und bilder

--- src/de/fh/multimedia/bmp/BMPStarter.java
+++ src/de/fh/multimedia/bmp/BMPStarter.java
@@ -1,10 +1,38 @@
 package de.fh.multimedia.bmp;
 
 public class BMPStarter {
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		File input = new File(BASEPATH+INPUTIMAGE);
+		int arrayRGB[][];
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(input);
+			arrayRGB = new int[image.getWidth()][image.getHeight()];
+			for (int xPixel = 0; xPixel < image.getWidth(); xPixel++)
+	        {
+	            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++)
+	            {
+	               arrayRGB[xPixel][yPixel] = image.getRGB(xPixel, yPixel);
+	               System.out.print(image.getRGB(xPixel, yPixel)+"\t");	 
+	               image.setRGB(xPixel, yPixel, Color.BLACK.getRGB());
+	            }
+	            System.out.println();
+	        }
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		File output = new File(OUTPUTPATH+OUTPUTIMAGE);
+		try {
+			ImageIO.write(image, "bmp", output);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
