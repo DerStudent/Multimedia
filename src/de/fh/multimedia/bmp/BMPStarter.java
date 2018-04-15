a800fa5287ca0832e881f2557d406cd1533c1c02
DerStudent <strolsu@googlemail.com>
2018-04-15 15:14:28 +0100

    PATH angeglichen

--- src/de/fh/multimedia/bmp/BMPStarter.java
+++ src/de/fh/multimedia/bmp/BMPStarter.java
@@ -10,41 +10,47 @@ import javax.imageio.ImageIO;
 public class BMPStarter {
 	
 	
-	private static final String BASEPATH = "";
-	private static final String INPUTIMAGE = "";
+	private static final String BASEPATH = "D:\\Projekte\\Multimedia\\Bilder\\";
+	private static final String INPUTIMAGE = "Image.bmp";
 	
-	private static final String OUTPUTPATH = "";
-	private static final String OUTPUTIMAGE = "";
+	private static final String OUTPUTPATH = "D:\\Projekte\\Multimedia\\Bilder\\";
+	private static final String OUTPUTIMAGE = "ImageNew.bmp";
+	
+	private static final int COLOR = Color.BLACK.getRGB();
 
 	public static void main(String[] args) {
 		
 		File input = new File(BASEPATH+INPUTIMAGE);
+		
 		int arrayRGB[][];
+		
 		BufferedImage image = null;
+		
 		try {
 			image = ImageIO.read(input);
 			arrayRGB = new int[image.getWidth()][image.getHeight()];
+			
 			for (int xPixel = 0; xPixel < image.getWidth(); xPixel++)
 	        {
 	            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++)
 	            {
-	               arrayRGB[xPixel][yPixel] = image.getRGB(xPixel, yPixel);
-	               System.out.print(image.getRGB(xPixel, yPixel)+"\t");	 
-	               image.setRGB(xPixel, yPixel, Color.BLACK.getRGB());
+	            	int rgbCode = image.getRGB(xPixel, yPixel);
+	            	arrayRGB[xPixel][yPixel] = rgbCode;
+	               	System.out.print(rgbCode); 
+	               	image.setRGB(xPixel, yPixel, COLOR);
 	            }
 	            System.out.println();
 	        }
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 		
 		File output = new File(OUTPUTPATH+OUTPUTIMAGE);
 		try {
 			ImageIO.write(image, "bmp", output);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
-		}
+		}	
 	}
-
 }
